e5c62cccfeb8c670c59cb3f2bc9df6130eea3f82
Marcus Ng <ngzhengjie888@gmail.com>
2025-08-29 17:20:25 +0800

    edited the hardcoded backend localhost url

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,7 +29,8 @@ class Settings(BaseSettings):
             "http://localhost:3001", 
             "http://127.0.0.1:3000", 
             "http://127.0.0.1:3001",
-            "https://5d7a587ce8ba.ngrok-free.app"
+            "https://5d7a587ce8ba.ngrok-free.app",
+            "https://bos-solution.onrender.com"
         ],
         env="ALLOWED_HOSTS"
     )
